06f8361c5238ff68243a7c8f1e76a03c7bae91e6
rita <ritaalfy@gmail.com>
2023-10-09 15:56:09 +0200

    ref page faite

--- alyfpro_project/src/Controller/HomeController.php
+++ alyfpro_project/src/Controller/HomeController.php
@@ -22,9 +22,18 @@ class HomeController extends AbstractController
     {
         return $this->render('front/pages/about.html.twig');
     }
+
     #[Route('/services', name: 'app_services')]
     public function service(): Response
     {
         return $this->render('front/pages/service.html.twig');
+    }
+    #[Route('/references', name: 'app_references')]
+    public function reference(): Response
+    {
+        $imageNames = ['m2i.png', 'isitech.png', 'president.png', 'delcoupe.png', 'oip.png','orsys.png','ort.png','fitech.png', 'mga.png', 'edu.png'];
+        return $this->render('front/pages/references.html.twig', [
+            'imageNames' => $imageNames,
+        ]);
     }
 }
