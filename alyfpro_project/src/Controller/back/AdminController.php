f5795f26221140bf40f0525dce603387a27bf5ed
rita <ritaalfy@gmail.com>
2023-11-17 15:34:41 +0100

    dashboard table 4users

---
+++ alyfpro_project/src/Controller/back/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\back;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(
+        UserRepository $userRepository,
+
+    ): Response
+    {
+        return $this->render('back/admin/dashboard.html.twig', [
+
+            'users' => $userRepository->findBy([], ['lastName' => 'Asc'], 4),
+        ]);
+    }
+}
