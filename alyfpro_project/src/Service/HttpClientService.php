cc09b007388ab729b6bf432e53bae8d750fde5bd
rita <ritaalfy@gmail.com>
2023-11-22 11:58:30 +0100

    httpclient service

---
+++ alyfpro_project/src/Service/HttpClientService.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Service;
+
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+
+class HttpClientService
+{
+
+    /**
+     * @param string $url
+     * @return ResponseInterface
+     * @throws TransportExceptionInterface
+     */
+    public function get(string $url): ResponseInterface
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', $url);
+
+        if (200 !== $response->getStatusCode()) {
+            throw new Exception('The API return an error.');
+        }
+
+        return $response;
+    }
+
+}
